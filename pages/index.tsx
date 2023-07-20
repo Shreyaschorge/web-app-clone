cbb1fe507a382c755a3998fa640d944475e04a66
Shreyaschorge <shreyaschorge81@gmail.com>
2023-07-20 22:26:36 +0530

    log the vercel url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import ProjectsList from '../src/features/projects/screens/Projects';
-import GetAllProjectsMeta from '../src/utils/getMetaTags/GetAllProjectsMeta';
-import getStoredCurrency from '../src/utils/countryCurrency/getStoredCurrency';
-import { getRequest } from '../src/utils/apiRequests/api';
-import { ProjectPropsContext } from '../src/features/common/Layout/ProjectPropsContext';
-import Credits from '../src/features/projects/components/maps/Credits';
-import Filters from '../src/features/projects/components/projects/Filters';
-import { TENANT_ID } from '../src/utils/constants/environment';
-import { ErrorHandlingContext } from '../src/features/common/Layout/ErrorHandlingContext';
-import DirectGift from '../src/features/donations/components/DirectGift';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
-import nextI18NextConfig from '../next-i18next.config';
-import { handleError, APIError } from '@planet-sdk/common';
+import { useRouter } from "next/router";
+import React from "react";
+import ProjectsList from "../src/features/projects/screens/Projects";
+import GetAllProjectsMeta from "../src/utils/getMetaTags/GetAllProjectsMeta";
+import getStoredCurrency from "../src/utils/countryCurrency/getStoredCurrency";
+import { getRequest } from "../src/utils/apiRequests/api";
+import { ProjectPropsContext } from "../src/features/common/Layout/ProjectPropsContext";
+import Credits from "../src/features/projects/components/maps/Credits";
+import Filters from "../src/features/projects/components/projects/Filters";
+import { TENANT_ID } from "../src/utils/constants/environment";
+import { ErrorHandlingContext } from "../src/features/common/Layout/ErrorHandlingContext";
+import DirectGift from "../src/features/donations/components/DirectGift";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import nextI18NextConfig from "../next-i18next.config";
+import { handleError, APIError } from "@planet-sdk/common";
 
 interface Props {
   initialized: Boolean;
@@ -39,12 +39,12 @@ export default function Donate({
   const { redirect, setErrors } = React.useContext(ErrorHandlingContext);
   const { i18n } = useTranslation();
   const router = useRouter();
-  const [internalCurrencyCode, setInternalCurrencyCode] = React.useState('');
+  const [internalCurrencyCode, setInternalCurrencyCode] = React.useState("");
   const [directGift, setDirectGift] = React.useState(null);
   const [showdirectGift, setShowDirectGift] = React.useState(true);
-  const [internalLanguage, setInternalLanguage] = React.useState('');
+  const [internalLanguage, setInternalLanguage] = React.useState("");
   React.useEffect(() => {
-    const getdirectGift = localStorage.getItem('directGift');
+    const getdirectGift = localStorage.getItem("directGift");
     if (getdirectGift) {
       setDirectGift(JSON.parse(getdirectGift));
     }
@@ -58,10 +58,12 @@ export default function Donate({
     }
   }, [directGift]);
 
+  console.log(process.env.NEXTAUTH_URL);
+
   // Deprecation Notice: This route will be removed in next major version
   React.useEffect(() => {
     if (router.query.p) {
-      router.push('/[p]', `/${router.query.p}`, {
+      router.push("/[p]", `/${router.query.p}`, {
         shallow: true,
       });
     }
@@ -87,10 +89,10 @@ export default function Donate({
         setInternalLanguage(i18n.language);
         try {
           const projects = await getRequest(`/app/projects`, {
-            _scope: 'map',
+            _scope: "map",
             currency: currency,
             tenant: TENANT_ID,
-            'filter[purpose]': 'trees,conservation',
+            "filter[purpose]": "trees,conservation",
             locale: i18n.language,
           });
           setProjects(projects);
@@ -99,7 +101,7 @@ export default function Donate({
           setZoomLevel(1);
         } catch (err) {
           setErrors(handleError(err as APIError));
-          redirect('/');
+          redirect("/");
         }
       }
     }
@@ -149,27 +151,27 @@ export async function getStaticProps({ locale }: any) {
       ...(await serverSideTranslations(
         locale,
         [
-          'bulkCodes',
-          'common',
-          'country',
-          'donate',
-          'donationLink',
-          'editProfile',
-          'giftfunds',
-          'leaderboard',
-          'managePayouts',
-          'manageProjects',
-          'maps',
-          'me',
-          'planet',
-          'planetcash',
-          'redeem',
-          'registerTrees',
-          'tenants',
-          'treemapper',
+          "bulkCodes",
+          "common",
+          "country",
+          "donate",
+          "donationLink",
+          "editProfile",
+          "giftfunds",
+          "leaderboard",
+          "managePayouts",
+          "manageProjects",
+          "maps",
+          "me",
+          "planet",
+          "planetcash",
+          "redeem",
+          "registerTrees",
+          "tenants",
+          "treemapper",
         ],
         nextI18NextConfig,
-        ['en', 'de', 'fr', 'es', 'it', 'pt-BR', 'cs']
+        ["en", "de", "fr", "es", "it", "pt-BR", "cs"]
       )),
     },
   };
