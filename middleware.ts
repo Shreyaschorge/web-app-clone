a5e7f80550518a11d4a7aca5bc88719125790263
Shreyaschorge <shreyaschorge81@gmail.com>
2023-08-23 11:49:35 +0530

    remove profile from matcher list

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getTenantSubdomainOrDefault } from './src/utils/db';
 
 export const config = {
-  matcher: ['/', '/_sites/:path', '/profile'],
+  matcher: ['/', '/_sites/:path'],
 };
 
 export default async function middleware(req: NextRequest) {
